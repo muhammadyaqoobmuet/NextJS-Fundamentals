fd07014b26738bb8649f71cfab559511b36728f9
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-07 17:52:29 +0500

    some changes to create a post

--- app/posts/create/page.js
+++ app/posts/create/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 function page() {
     return (
         <div>
+        <h1 className='text-white'>create a new post</h1>
             <input type='text' />
         </div>
     )
