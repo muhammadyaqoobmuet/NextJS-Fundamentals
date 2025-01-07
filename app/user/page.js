5036099524743ffc3fa1f980df1cd3adabb89dfe
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-07 16:53:53 +0500

    file based routing with ServerSIde compos

---
+++ app/user/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function userPage() {
+    return (
+        <div className='max-w-md mx-auto mt-20 rounded-2xl shadow-md h-32 shadow-gray-500 flex items-center justify-center border bg-white'>
+            <h1 className='text-black text-2xl font-bold font-[family-name:var(--font-geist-sans)]'>User</h1>
+        </div>
+    )
+}
+
+export default userPage
