4f5e95ba7d10cf60a449d2e5ab14fb98da2e2679
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-07 16:56:26 +0500

    creating nested routes for posts and new posts

---
+++ app/posts/create/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>
+            <input type='text' />
+        </div>
+    )
+}
+
+export default page
---
+++ app/posts/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div >
+        <div>POSTS</div>
+    </div>
+  )
+}
+
+export default page
