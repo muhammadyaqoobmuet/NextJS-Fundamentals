e595833c3931c24d0d8da2b3ac96151542efeb65
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-07 18:20:46 +0500

    added example of SSR server side rendering

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -1,26 +1,41 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+
+import React from 'react';
 import PostLoading from './loading'
 
-function page({ params }) {
-    const [load, setLoading] = useState(true)
+async function page({ params }) {
+    // const [load, setLoading] = useState(true)
     /**
         React.use() is a special hook that Next.js provides for unwrapping async data. It
         lets you handle promises correctly when working with the App Router, which is why you need to use it to access params safely. 
 
+        // we will use useEffect or useState in client side component 
+
+
+    1. SSR (Server-Side Rendering)
+
+   -> Purpose: Fetch data on each request. 
+** How it works: **
+    With SSR, the page is rendered on the server for every request that comes in. When a user visits a page,
+  Next.js will fetch the data, generate the HTML, and then send it to the client.
+ This happens on each request, ensuring that the data is always up-to-date.
+
+    ** When to use it: **
+Use SSR when your page content needs to be fresh with every request and must reflect real-time data (e.g., user profiles).
+
      */
+    const postId = params.postId;
 
-
-    const postId = React.use(params).postId;
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 2000)
-    }, [])
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    const data = await response.json()
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setLoading(false)
+    //     }, 2000)
+    // }, [])
     return (
-        load ? <PostLoading /> : <div>
-            you are visiting blog post of {postId}
+        <div>
+            <h1>{data.title}</h1>
+            <p>{data.body}</p>
         </div>
     )
 }
